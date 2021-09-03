7a303208c67d8baf3e5fd705bfc0e7f99d904562
Willian Silva <silva.williansantos@gmail.com>
2021-09-03 09:27:31 -0300

    Setup forecast client

---
+++ src/clients/__tests__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { StormGlass } from '@src/clients/stormGlass';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalizedResponse3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  it('Return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalizedResponse3HoursFixture);
+  });
+});
