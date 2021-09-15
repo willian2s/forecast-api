8be7bb3d0482e9a799d79bcf3348c79513f78a4f
Willian Silva <silva.williansantos@gmail.com>
2021-09-15 09:31:46 -0300

    :new:Add validation error

---
+++ src/controllers/index.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express';
+import mongoose from 'mongoose';
+
+export abstract class BaseController {
+  protected sendCreateUpdateErrorResponse(
+    res: Response,
+    error: mongoose.Error.ValidationError | Error
+  ): void {
+    if (error instanceof mongoose.Error.ValidationError) {
+      console.log(error.stack);
+      res.status(422).send({ code: 422, error: error.message });
+    } else {
+      res.status(500).send({ code: 500, error: 'Something went wrong!' });
+    }
+  }
+}
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -13,5 +13,18 @@ describe('Users functional tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
+
+    it('Return 422 when there is as validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '12345',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
   });
 });
